25ddb3bdab777139bf8ee7ba00da50715a75a40d
njckf2k <44032235+njckf2k@users.noreply.github.com>
2019-10-25 00:15:43 +0700

    numpy - 25/10 - 00:15

--- MachineLearningTutorial/ml_modul/matplotlib_show.py
+++ MachineLearningTutorial/ml_modul/matplotlib_show.py
@@ -5,11 +5,21 @@ from  matplotlib.figure import Figure
 import numpy as np
 
 #line plot - kieu lien
-plt.plot(np.random.randint(20, size=100)) #1 vector 1d, co 100 gia tri < 20
-plt.show()
+# plt.plot(np.random.randint(20, size=100)) #1 vector 1d, co 100 gia tri < 20
+# plt.show()
+
 #scatter plot- do thi kieu roi rac
-x = np.random.randn(300)
-y = np.random.randn(300)
-# plt.plot(x,y,"ob")
-plt.scatter(x,y)
+# x = np.random.randn(300)
+# y = np.random.randn(300)
+# # plt.plot(x,y,"ob")
+# plt.scatter(x,y)
+# plt.show()
+
+#bar chart
+y = [3,5,10,2,1,9,6]
+N = len(y)
+x = range(N)
+width = 1/1.5
+plt.bar(x,y,width,color = "blue")
 plt.show()
+
